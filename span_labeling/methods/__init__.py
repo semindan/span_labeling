814b1edd8d9ff373eda2013a11f3b7ae27976afb
Zdenek Kasner <zdenek.kasner@gmail.com>
2025-10-31 17:54:46 +0100

    Black formatting

--- span_labeling/methods/__init__.py
+++ span_labeling/methods/__init__.py
@@ -1,8 +1,15 @@
-from . import index_method, occurrence_method, xml_method, json_method
+from . import (
+    constrained_xml_method,
+    index_method,
+    json_method,
+    occurrence_method,
+    xml_method,
+)
 
 __all__ = [
     "index_method",
     "occurrence_method",
     "xml_method",
     "json_method",
+    "constrained_xml_method",
 ]
